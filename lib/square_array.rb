ac5d7fc12086c05edcdf7553fe88308d5883a059
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-19 05:29:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  new_array = []
+  counter = 0
+  while(counter < array.length) do
+    new_array[counter] = Math.pow(array[counter],2)
+    counter+=1
+  end
 end
+
